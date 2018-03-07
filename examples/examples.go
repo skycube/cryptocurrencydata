ef1cb2610a80535671461120fe3506fd98add043
skycube <plbaasch@gmail.com>
2018-03-07 21:47:33 +1000

    travis adjustments

--- examples/examples.go
+++ examples/examples.go
@@ -38,4 +38,14 @@ func main() {
 		fmt.Printf("%v\n", v)
 	}
 
+	// Get single currency history
+	currencyHistory, err := ccdata.GetCurrencyHistoryByCurrencyID("skycoin")
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	fmt.Println("# GetCurrencyHistoryByCurrencyID")
+	for _, v := range currencyHistory {
+		fmt.Printf("timestamp %v timeUTC: %v MarketSupply: %d PriceUSD: $%.2f PriceBTC: $%.2f VolUSD: $%d\n", v.Timestamp, v.TimeUTC, v.MarketSupply, v.PriceUSD, v.PriceBTC, v.VolUSD)
+	}
+
 }
